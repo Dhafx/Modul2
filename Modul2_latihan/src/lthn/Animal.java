3d393fcc5da80bb579465fa5f44cf40ba26195bc
M. Dhafa Maulana <120663181+Dhafx@users.noreply.github.com>
2023-03-24 15:29:24 +0700

    Add files via upload

---
+++ Modul2_latihan/src/lthn/Animal.java
@@ -0,0 +1,19 @@
+package lthn;
+
+public class Animal {
+    private String name;
+    private String color;
+
+    public Animal(String name, String color) {
+        this.name = name;
+        this.color = color;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+}
---
+++ Modul2_latihan/src/lthn/DeleteAnimal.java
@@ -0,0 +1,13 @@
+package lthn;
+
+public class DeleteAnimal {
+    private String color;
+
+    public DeleteAnimal(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+}
